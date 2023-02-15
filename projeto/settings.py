228fb3a580075b0f2d5b783c41ccd2b2ca98ffdb
EsterMel0 <estefanymelo028@gmail.com>
2023-02-15 15:47:29 -0400

    enviando novos arquivos do site

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes', #temos que infromar ao django que tambem temos apps
 ]
 
 MIDDLEWARE = [
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request,'home.html', context={
+        'name':'Estefany Melo'
+    })
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
+
+def sobre(request):
+    return HttpResponse("SOBRE")
