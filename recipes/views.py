dfb2c0d74edeb1d4d1d1a268730f2f50386ee626
EsterMel0 <estefanymelo028@gmail.com>
2023-02-15 16:06:15 -0400

    excluindo contato/ e sobre/

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
     return render(request,'home.html', context={
         'name':'Estefany Melo'
     })
-
-
-def contato(request):
-    return render(request,'contato.html')
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
