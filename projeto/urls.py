fbca4f19608e5fa72f13383c722008352687f6f6
EsterMel0 <estefanymelo028@gmail.com>
2023-02-15 13:21:37 -0400

    inserindo funções do site

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,11 +18,23 @@ from django.http import HttpResponse
 from django.urls import path
 
 
-def my_view(request):
-    return HttpResponse("UMA STRING")
+
+def home(request):
+    return HttpResponse("HOME")
+
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
+
+def sobre(request):
+    return HttpResponse("SOBRE")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", my_view),
+    path('', home), #home
+    path('contato/', contato), #contato/
+    path('sobre/', sobre), #sobre/
 ]
