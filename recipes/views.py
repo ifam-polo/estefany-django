d319835ce83ef5352b24e030d02941fe0c8b9c43
EsterMel0 <estefanymelo028@gmail.com>
2023-02-16 14:21:10 -0400

    enviando partials do site

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'home.html', context={
+    return render(request,'recipes/pages/home.html', context={
         'name':'Estefany Melo'
     })
