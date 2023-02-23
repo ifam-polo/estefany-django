07c5ba6efb3207b33fbeda6226919254f9492ea8
EsterMel0 <estefanymelo028@gmail.com>
2023-02-23 12:01:44 -0400

    enviando alterações

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request,'recipes/pages/home.html', context={
         'name':'Estefany Melo'
     })
+
+
+def recipe(request, id):
+    return render(request,'recipes/pages/recipe-view.html', context={
+        'name':'Estefany Melo'
+    })
