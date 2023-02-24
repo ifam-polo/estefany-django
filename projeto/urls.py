a76125d2ad920c3c032b78c4785917d9928860c2
EsterMel0 <estefanymelo028@gmail.com>
2023-02-24 17:01:40 -0400

    enviando alterações

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -22,3 +24,7 @@ urlpatterns = [
     path('',include('recipes.urls')),
     path('recipes/', include('recipes.urls')), #dominio.com/recipes/
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home), #home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="recipes-home"), #home
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"), #details
 ]
