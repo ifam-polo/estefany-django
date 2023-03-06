4ab945aa61c17fa78cae77b59b093422288118d5
EsterMel0 <estefanymelo028@gmail.com>
2023-03-06 16:54:11 -0400

    enviando correções

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,4 +1,4 @@
-"""projeto URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,16 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
-
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('recipes.urls')),
-    path('recipes/', include('recipes.urls')), #dominio.com/recipes/
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),  # dominio.com/recipes/
 ]
 
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import get_list_or_404, render
-from utils.recipes.factory import make_recipe
-from recipes.models import Recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
+from recipes.models import Recipe
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes =  Recipe.objects.filter(
+    recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
-    return render(request,'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
@@ -17,22 +17,19 @@ def category(request, category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category_id=category_id,
-            is_published = True,
+            is_published=True,
         ).order_by('-id')
     )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title' : f'{recipes[0].category.name} - Category |'
+        'title': f'{recipes[0].category.name} - Category |'
     })
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        id=id,
-        is_published=True,
-    ).order_by('-id').first()
-    return render(request,'recipes/pages/recipe-view.html', context={
-        'recipe':recipe,
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
         'is_detail_page': True,
     })
