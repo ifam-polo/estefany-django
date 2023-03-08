0d078a5066b9a6b9f188ff3057909054d3231a43
EsterMel0 <estefanymelo028@gmail.com>
2023-03-08 16:59:28 -0400

    ajustes no css

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
+
+from recipes import views
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"), #home
-    path('recipes/category/<int:category_id>/', views.category, name="category"), 
-    path('recipes/<int:id>/', views.recipe, name="recipe"), #details
-   
+    path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
+
 ]
