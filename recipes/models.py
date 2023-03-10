3b4ae2c923c7d206fb33160ef70284471aa42c54
EsterMel0 <estefanymelo028@gmail.com>
2023-03-10 11:42:55 -0400

    enviando correção no teste da home

--- recipes/models.py
+++ recipes/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
-    
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -26,12 +27,10 @@ class Recipe(models.Model):
         upload_to='recipes/covers/%y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
-        )
+    )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
-        )
+    )
 
     def __str__(self):
         return self.title
-    
-
