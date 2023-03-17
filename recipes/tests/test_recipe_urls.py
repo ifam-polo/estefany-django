1c55058e06ce6dcdd9deb9a619942e1a704676dd
EsterMel0 <estefanymelo028@gmail.com>
2023-03-17 12:06:00 -0400

    sem alterações no projeto

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -18,3 +18,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_search_url_is_correct(self):
         url = reverse('recipes:search')
         self.assertEqual(url, '/recipes/search/')
+
+# TDD:red - testar o teste;
+#     green - o teste precisa passar;
+#     refactor - refazer o ciclo;
