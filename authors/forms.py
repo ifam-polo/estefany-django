9638147d4a67358698110ba995241273d4fd24bc
EsterMel0 <estefanymelo028@gmail.com>
2023-04-11 15:34:34 -0400

    configurando o formulário

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,31 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        labels = {
+            'first_name': 'first_name',
+            'last_name': 'last_name',
+            'username': 'username',
+            'email': 'email',
+            'password': 'password',
+        }
+
+        help_texts = {
+            'email': 'The e-mail must be valid.'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here',
+                'class': 'input text-input outra-coisa'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
