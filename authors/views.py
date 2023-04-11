bfc1c2b8bfb706f0c75645aa63d616cecbea63a6
EsterMel0 <estefanymelo028@gmail.com>
2023-04-11 15:15:56 -0400

    adicionando css ao formulário

--- authors/views.py
+++ authors/views.py
@@ -1,11 +1,12 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
@@ -15,7 +16,8 @@ def register_create(request):
     if not request.POST:
         raise Http404()
 
-    form = RegisterForm(request.POST)
-    return render(request, 'authors/pages/register_view.html', {
-        'form': form,
-    })
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
