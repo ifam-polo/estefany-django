7c98ad5a207b8dee8df8b21b754e0e0ef0297aab
EsterMel0 <estefanymelo028@gmail.com>
2023-04-12 14:43:44 -0400

    inserindo validação no campo de password

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
@@ -78,3 +79,14 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password': 'Password and password2 must be equal'
+            })
