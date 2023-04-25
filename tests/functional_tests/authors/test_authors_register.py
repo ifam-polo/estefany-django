08dd8e43acbaf443789f1f879abdbe078701b847
EsterMel0 <estefanymelo028@gmail.com>
2023-04-25 17:44:59 -0400

    testando campos do formulário de registro

---
+++ tests/functional_tests/authors/test_authors_register.py
@@ -0,0 +1,71 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from .base import AuthorsBaseTest
+
+
+class AuthorsRegisterTest(AuthorsBaseTest):
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]'
+        )
+
+    def fill_form_dummy_data(self, form):
+        fields = form.find_elements(By.TAG_NAME, 'input')
+
+        for field in fields:
+            if field.is_displayed():
+                field.send_keys(' '*20)
+
+    def get_form(self):
+        return self.browser.find_element(
+            By.XPATH,
+            'html/body/main/div[2]/form'
+        )
+
+    def form_field_test_with_callback(self, callback):
+        self.browser.get(self.live_server_url + '/authors/register/')
+        form = self.get_form()
+
+        self.fill_form_dummy_data
+        form.find_element(By.NAME, 'email').send_keys('dummy@email.com')
+
+        callback(form)
+
+        return form
+
+    def test_empty_first_name_message(self):
+        def callback(form):
+            first_name_field = self.get_by_placeholder(form, 'Ex.: John')
+            first_name_field.send_keys(' ')
+            first_name_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('Write your first name', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_empty_last_name_message(self):
+        def callback(form):
+            last_name_field = self.get_by_placeholder(form, 'Ex.: Doe')
+            last_name_field.send_keys(' ')
+            last_name_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('Write your last name', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_empty_username_error_message(self):
+        def callback(form):
+            username_field = self.get_by_placeholder(form, 'Your username')
+            username_field.send_keys(' ')
+            username_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('This field must not be empty', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_invalid_email_error_message(self):
+        def callback(form):
+            email_field = self.get_by_placeholder(form, 'Your e-mail')
+            email_field.send_keys('email@invalid')
+            email_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('The e-mail must be valid.', form.text)
+        self.form_field_test_with_callback(callback)
--- tests/recipes/test_recipe_home_page.py
+++ tests/recipes/test_recipe_home_page.py
@@ -10,7 +10,6 @@ from .base import RecipeBaseFunctionalTest
 # aula 167 - sobre makers
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    @patch('recipes.views.PER_PAGE', new=3)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
