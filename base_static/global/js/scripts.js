8dea07626899135d138626985fce937080857e2d
EsterMel0 <estefanymelo028@gmail.com>
2023-05-02 16:45:24 -0400

    adicionando JavaScript e corrigindo problemas de segurança no form

---
+++ base_static/global/js/scripts.js
@@ -0,0 +1,17 @@
+function my_scope(){
+    const form = document.querySelectorAll('.form-delete');
+
+    for(const form of forms){
+        form.addEventListener('submit', function(e){
+            e.preventDefault();
+
+            const confirmed = confirm('Are your sure?')
+
+            if (confirmed){
+                form.submit();
+            }
+        });
+    }
+}
+
+my_scope();
