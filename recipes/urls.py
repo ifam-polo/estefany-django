81c9154d0b5c2becc49b14a350df1ad64abfc2e6
EsterMel0 <estefanymelo028@gmail.com>
2023-05-05 10:53:02 -0400

    aplicando based view nas views category e search

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,4 @@
-
+# flake8: noqa
 from django.urls import path
 
 from . import views
@@ -6,9 +6,14 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.RecipeListViewBase.as_view(), name="home"),
-    path('recipes/search/', views.search, name='search'),
-    path('recipes/category/<int:category_id>/',
-         views.category, name="category"),
+    path('', views.RecipeListViewHome.as_view(), name="home"),
+    path(
+        'recipes/search/', 
+        views.RecipeListViewSearch.as_view(), name='search'
+    ),
+    path(
+        'recipes/category/<int:category_id>/',
+         views.RecipeListViewCategory.as_view(), name="category"
+    ),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
