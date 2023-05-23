b08bd1df45747d449585e0ebb96fdf3cda3ac24a
EsterMel0 <estefanymelo028@gmail.com>
2023-05-23 17:41:54 -0400

    finalizando seção 28

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -2,8 +2,8 @@
 from .environment import *  # isort:skip
 from .installed_apps import *  # isort:skip
 from .middlewares import *  # isort:skip
-
 from .assets import *
+from .cors_headers import *
 from .databases import *
 from .i18n import *
 from .messages import *
